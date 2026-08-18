a7cea0f1ef21c934223e70335fd00743fef881ab
FeFe <90733495+squaredfe@users.noreply.github.com>
2026-08-18 10:09:46 -0300

    dsfsdafasfdasfd

---
+++ public/login.js
@@ -0,0 +1,26 @@
+async function login(dni, passwd) {
+        const res = await fetch("https://totake-be.onrender.com/api/v1/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                dni: dni,
+                passwd: passwd
+                /* porque pinto */
+            })
+            });
+            const data = await res.json();
+            if (res.ok) {
+                console.log("Token:", data.token);
+                localStorage.setItem("token", data.token); // guardalo para requests futuros
+            } else {
+                console.error(data.error);
+            }
+        }
+
+
+function loginPress(){
+    var inputDni = document.getElementById("labelDni").text;
+    var inputPasswd = document.getElementById("labelPasswd").text;
+    login(inputDni, inputPasswd);
+}
+document.getElementById("loginButton").onclick = loginPress();
