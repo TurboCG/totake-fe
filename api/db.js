295c0321cb3f528231eaffecdd3eaa1d7c0cc01c
FeFe <90733495+squaredfe@users.noreply.github.com>
2026-08-18 09:45:05 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -2,4 +2,4 @@ import { neon } from '@neondatabase/serverless';
 // La connection string te la da Neon en su dashboard
 const sql = neon(process.env.DATABASE_URL);
 
-exports = { sql };
+module.exports = { sql };
