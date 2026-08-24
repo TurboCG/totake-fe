56b4d4652c4c1747cfbd5969bc6c749f83b70a26
FeFe <90733495+squaredfe@users.noreply.github.com>
2026-08-24 08:44:08 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,6 +1,6 @@
 async function login(dni, passwd) {
     try{
-    const res = await fetch("https://totake-be.onrender.com/api/v1/login", {
+    const response = await fetch("https://totake-be.onrender.com/api/v1/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
