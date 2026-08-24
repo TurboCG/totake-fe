f125e1d25cf730eff0cdaa6e695208977f06b498
FeFe <90733495+squaredfe@users.noreply.github.com>
2026-08-24 08:29:32 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,21 +1,21 @@
 async function login(dni, passwd) {
-        const res = await fetch("https://totake-be.onrender.com/api/v1/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-                dni: dni,
-                passwd: passwd
-                /* porque pinto */
-            })
-            });
-            const data = await res.json();
-            if (res.ok) {
-                console.log("Token:", data.token);
-                localStorage.setItem("token", data.token); // guardalo para requests futuros
-            } else {
-                console.error(data.error);
-            }
+    const res = await fetch("https://totake-be.onrender.com/api/v1/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            dni: dni,
+            passwd: passwd
+            /* porque pinto */
+        })
+        });
+        const data = await res.json();
+        if (res.ok) {
+            console.log("Token:", data.token);
+            localStorage.setItem("token", data.token); // guardalo para requests futuros
+        } else {
+            console.error(data.error);
         }
+    }
 
 
 function loginPress(){
@@ -23,4 +23,4 @@ function loginPress(){
     var inputPasswd = document.getElementById("labelPasswd");
     login(inputDni, inputPasswd);
 }
-document.getElementById("loginButton").onclick = loginPress();
+document.getElementById("loginButton").onclick = loginPress;
