d583e12ddca02f6ba7cd620bcd54257e119a0ee1
squaredfe <corporativo.mirtop@gmail.com>
2026-08-24 20:07:58 -0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ import { sql } from "./db.js";
 // Create an Express application instance
 const app = express();
 const users = [];
+app.use(express.json()); 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
