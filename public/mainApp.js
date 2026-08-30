9cbd75de45007581e6256eb0b04a9535cb75674d
squaredfe <corporativo.mirtop@gmail.com>
2026-08-30 18:22:04 -0300

    Update mainApp.js

--- public/mainApp.js
+++ public/mainApp.js
@@ -1,6 +1,6 @@
 let token;
 document.addEventListener("DOMContentLoaded", () => {
-    const token = localStorage.getItem("token");
+    token = localStorage.getItem("token");
 
     if (!token) {
         window.location.href = "/mainpage.html";
